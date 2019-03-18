7c274d430b40f6a987089395e96d0b3c93d910d8
monirul89 <monirul89@gmail.com>
2019-03-18 22:55:14 +0600

    no message

--- index.php
+++ index.php
@@ -1,17 +1,72 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
+ * @subpackage Shafidi
+ * @since 1.0
+ * @version 1.0
  */
+get_header();
+?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<div class="wrap">
+    <div class="container">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
+                <div class="col-sm-8">
+                    <section>
+                        <?php if (have_posts()) : ?>
+
+                            <?php if (is_home() && !is_front_page()) : ?>
+                                <header>
+                                    <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                                </header>
+                            <?php endif; ?>
+
+                            <?php
+                            // Start the loop.
+                            while (have_posts()) : the_post();
+
+                                /*
+                                 * Include the Post-Format-specific template for the content.
+                                 * If you want to override this in a child theme, then include a file
+                                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                                 */
+                                get_template_part('template-parts/content', get_post_format());
+
+                            // End the loop.
+                            endwhile;
+
+                            // Previous/next page navigation.			
+                            the_posts_pagination(array(
+                                'mid_size' => 2,
+                                'prev_text' => __('Previous page', 'shafidi'),
+                                'next_text' => __('Next page', 'shafidi'),
+                            ));
+
+                        // If no content, include the "No posts found" template.
+                        else :
+                            get_template_part('template-parts/content', 'none');
+
+                        endif;
+                        ?>
+                    </section>
+                </div>
+                <div class="col-sm-4">
+                    <?php get_sidebar(); ?>
+                </div>
+            </main><!-- #main -->
+        </div><!-- #primary -->
+
+    </div><!-- .wrap -->
+<?php
+get_footer();
